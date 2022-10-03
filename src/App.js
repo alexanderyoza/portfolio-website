c25a19df8a41c331b8cdcaf56d68f06d48b0be21
alexanderyoza <alex.yoza@gmail.com>
2022-10-02 17:05:48 -0700

    skills and exp

--- src/App.js
+++ src/App.js
@@ -2,7 +2,21 @@ import logo from './logo.svg';
 import './App.css';
 import Top from './components/Top/Top';
 import Card from './components/Card/Card';
+
 import alex from './images/alex.png';
+import angular from './images/angular.png';
+import boh from './images/boh.png';
+import c from './images/c.png';
+import express from './images/express.png';
+import htmlcss from './images/htmlcss.png';
+import java from './images/java.png';
+import javascript from './images/javascript.png';
+import mongo from './images/mongo.png';
+import node from './images/node.png';
+import projdest from './images/projdest.png';
+import python from './images/python.png';
+import react from './images/react.png';
+import typescript from './images/typescript.png';
 
 function App() {
   // title, description, imagelink on each inner array
@@ -23,25 +37,72 @@ function App() {
 
   const skill = [
     [
-
+      'Java',
+      'desc',
+      java
     ],
     [
-    
+      'Python',
+      'desc',
+      python
     ],
     [
-
+      'C',
+      'desc',
+      c
+    ],
+    [
+      'HTML/CSS',
+      'desc',
+      htmlcss
+    ],
+    [
+      'Javascript',
+      'desc',
+      javascript
+    ],
+    [
+      'Typescript',
+      'desc',
+      typescript
+    ],
+    [
+      'Angular',
+      'desc',
+      angular
+    ],
+    [
+      'React',
+      'desc',
+      react
+    ],
+    [
+      'MongoDB',
+      'desc',
+      mongo
+    ],
+    [
+      'Express',
+      'desc',
+      express
+    ],
+    [
+      'Node',
+      'desc',
+      node
     ]
   ];
   const experience = [
     [
-
+      "Project Destined - Commercial Real Estate Intern",
+      'desc',
+      projdest
     ],
     [
-      
+      "Bank of Hawaii - eSolutions Development Intern",
+      'desc',
+      boh
     ],
-    [
-
-    ]
   ];
   const project = [
     [
