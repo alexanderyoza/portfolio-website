0538e4ea0e76ac0757042ca74c562b38a99143b2
alexanderyoza <alex.yoza@gmail.com>
2022-10-03 17:28:59 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -43,7 +43,15 @@ function App() {
     [
       'Java',
       'none',
-      <div>desc</div>,
+      <div>
+        Java is a programming language and computing 
+        platform first released by Sun Microsystems in 1995.
+        It has evolved from humble beginnings to power a large
+        share of today’s digital world, by providing the reliable
+        platform upon which many services and applications are built.
+        New, innovative products and digital services designed for
+        the future continue to rely on Java, as well. <a href="https://www.java.com/en/download/help/whatis_java.html" style={{textDecoration:'none', color:'#8BD8BD'}}>Learn More</a>
+      </div>,
       java,
       150
     ],
